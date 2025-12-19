fc44a4c2f2146dd1038a85007cab0bfa28aaa66d
haeing0v0 <sm134884@gmail.com>
2025-12-19 17:50:12 +0900

    12/19 commit

---
+++ day1219/src/operand/CompareOperator.java
@@ -0,0 +1,13 @@
+package operand;
+
+public class CompareOperator {
+
+	public static void main(String[] args) {
+
+		int v1 = 1;
+		double v2 = 1.0;
+		
+		System.out.println("==(연산) = " + (v1 == v2));
+	}
+
+}
