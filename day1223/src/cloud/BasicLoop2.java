78a0fe864d6bb22183c84b9d408074023d416d16
haeing0v0 <sm134884@gmail.com>
2025-12-23 12:50:04 +0900

    12/23 4-part commit

--- day1223/src/cloud/BasicLoop2.java
+++ day1223/src/cloud/BasicLoop2.java
@@ -6,10 +6,29 @@ public class BasicLoop2 {
 
 		//1~10 사이의 홀수를 구하는 코드를 작성하세요.(for문 사용)
 		
-		for (int i = 1; i <= 10; i += 2) {
-			
-			System.out.print(i);
+//		for (int i = 1; i <= 10; i += 2) {
+//			
+//			System.out.print(i);
+//		}
+		
+		int i, j, odd, odd1;
+		
+		for(i = 0, odd = 0; i < 10; i++) {
+			odd = i++;
+			System.out.print(", " + (odd + 1));
 		}
+		System.out.println();
 	
+		
+		for (j = 0, odd1 = 0; j < 10; j++) {
+			odd1 = j;
+			
+			if (j % 2 == 1) {
+				continue;
+			}
+			
+			System.out.print(", " + (odd1 + 1));
+			
+		}
 	}
 }
