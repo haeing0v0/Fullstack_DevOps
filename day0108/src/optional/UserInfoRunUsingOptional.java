bfc7afc960d8c7994ac83ce735da504860f4880e
haeing0v0 <sm134884@gmail.com>
2026-01-08 15:50:34 +0900

    01/08 6-part commit

---
+++ day0108/src/optional/UserInfoRunUsingOptional.java
@@ -0,0 +1,20 @@
+package optional;
+
+import java.lang.StackWalker.Option;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInfoRunUsingOptional {
+	
+	public static void usingOptional(ArrayList<UserInfo> list) {
+		Optional
+	}
+
+	public static void main(String[] args) {
+		ArrayList<UserInfo> userInfoList = new ArrayList<UserInfo>();
+		userInfoList.add(new UserInfo(12345, "James"));
+		userInfoList.add(new UserInfo(12346, "Tomas"));
+		userInfoList.add(new UserInfo(12347, "Edward"));
+	}
+
+}
--- day0108/src/stream/LimitRun.java
+++ day0108/src/stream/LimitRun.java
@@ -7,7 +7,12 @@ public class LimitRun {
 	public static void main(String[] args) {
 		
 		List<String> fruits = Arrays.asList("파인애플", "사과", "바나나", "귤");
-		System.out.println("기본정렬(오름차순 정렬) : ");
-		fruits.stream().sorted().limit(2).forEach(s -> System.out.println(s + " "));
+		System.out.print("기본정렬(오름차순 정렬) : ");
+		fruits.stream().sorted().limit(2).forEach(s -> System.out.print(s + " "));
+		System.out.println();
+		
+		System.out.print("맞춤형 정렬(문자 길이에 따라 정렬) : ");
+		fruits.stream().sorted((f1, f2) -> Integer.compare(f1.length(), f2.length()))
+		.limit(2).forEach(s -> System.out.print(s + " "));
 	}
 }
