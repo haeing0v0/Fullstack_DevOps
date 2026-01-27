5617bd8c5b647b7826c83fdfa11237b253e7da7f
haeing0v0 <sm134884@gmail.com>
2026-01-27 17:46:02 +0900

    01/27 OrderInsertRun update

--- day0127/src/org/cloud/order/main/OrdersInsertRun.java
+++ day0127/src/org/cloud/order/main/OrdersInsertRun.java
@@ -13,7 +13,7 @@ public class OrdersInsertRun {
 
         OrdersDTO dto = new OrdersDTO();
         dto.setOrder_num(20010);              
-        dto.setOrder_date(LocalDateTime.of(2004, 3, 27, 0, 0));
+        dto.setOrder_date(LocalDateTime.now());
         dto.setCust_id("1000000005");
 
         int result = dao.insert(dto);
