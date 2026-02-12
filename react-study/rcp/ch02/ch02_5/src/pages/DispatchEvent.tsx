20735b8e409c728efc8c1e390a9a8e342ee43de8
haeing0v0 <sm134884@gmail.com>
2026-02-12 17:49:48 +0900

    02/12 react-study

--- react-study/rcp/ch02/ch02_5/src/pages/DispatchEvent.tsx
+++ react-study/rcp/ch02/ch02_5/src/pages/DispatchEvent.tsx
@@ -1,3 +1,36 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+// export default function DispatchEvent() {
+//   const onCallDispatchEvent = () => {
+//     console.log('onCallDispatchEvent')
+//     document.getElementById('root')?.dispatchEvent(new Event('click', {bubbles: true}))
+//   }
+//   const onCallClick = () => {
+//     console.log('onCallClick')
+//     document.getElementById('root')?.click()
+//   }
+//   return (
+//     <div>
+//       <p>DispatchEvent</p>
+//       <button onClick={onCallDispatchEvent}>call dispatchEvent</button>
+//       <button onClick={onCallClick}>call click</button>
+//     </div>
+//   )
+// }
+
+export default function DispatchEvent() {
+  const onCallDispatchEvent = () => {
+    const event = new Event('click', {bubbles: true})
+    document.getElementById('root')?.addEventListener('click', e => {
+      e.stopPropagation()
+      console.log('bubbling stop')
+    })
+
+    document.getElementById('root')?.dispatchEvent(event)
+  }
+
+  return (
+    <div onClick={() => console.log('부모 태그')}>
+      <p>DispatchEvent</p>
+      <button onClick={onCallDispatchEvent}>call dispatchEvent</button>
+    </div>
+  )
 }
