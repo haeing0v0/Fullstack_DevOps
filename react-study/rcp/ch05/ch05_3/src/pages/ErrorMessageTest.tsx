ba7eb075839b469f92b686c6719e2232957ad327
haeing0v0 <sm134884@gmail.com>
2026-02-24 17:36:12 +0900

    02/24 ch05_3 practice:

--- react-study/rcp/ch05/ch05_3/src/pages/ErrorMessageTest.tsx
+++ react-study/rcp/ch05/ch05_3/src/pages/ErrorMessageTest.tsx
@@ -1,8 +1,33 @@
-export default function CopyMe() {
+import {useCallback} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import type {AppState} from '../store'
+import {Title} from '../components'
+import {Button} from '../theme/daisyui'
+import * as D from '../data'
+import * as E from '../store/errorMessage'
+
+export default function ErrorMessageTest() {
+  const dispatch = useDispatch()
+  const errorMessage = useSelector<AppState, E.State>(({errorMessage}) => errorMessage)
+
+  const generateErrorMessage = useCallback(() => {
+    dispatch<any>(E.generateErrorMessage(D.randomSentence()))
+  }, [dispatch])
+
+  // prettier-ignore
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ErrorMessageTest</Title>
+      <div className="mt-4">
+        <div className="flex justify-center mt-4">
+          <Button className="btn-sm btn-primary" onClick={generateErrorMessage}>
+            GENERATE ERROR MESSAGE
+          </Button>
+        </div>
+        <div className={`mt-4 p-4 flex items-center justify-center bg-red-200 ${errorMessage.length ? '' : 'invisible'}`}>
+          <p className="text-2xl text-red-600 text-bold">error: {errorMessage}</p>
+        </div>
+      </div>
     </section>
   )
 }
--- react-study/rcp/ch05/ch05_3/src/pages/FetchTest.tsx
+++ react-study/rcp/ch05/ch05_3/src/pages/FetchTest.tsx
@@ -1,8 +1,72 @@
-export default function CopyMe() {
+import {useState, useCallback, useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {useToggle} from '../hooks'
+import {Title, Avatar} from '../components'
+import {Button} from '../theme/daisyui'
+import * as D from '../data'
+import type {AppState} from '../store'
+import * as F from '../store/fetchUser'
+
+export default function RemoteUserTest() {
+  const dispatch = useDispatch()
+  const {
+    loading,
+    errorMessage,
+    fetchUser: user
+  } = useSelector<AppState, AppState>(state => state)
+
+  const getRemoteUser = useCallback(() => {
+    dispatch<any>(F.getRemoteUser())
+  }, [dispatch])
+  const changeName = useCallback(() => {
+    dispatch<any>(F.changeNameByFetching())
+  }, [dispatch])
+  const changeEmail = useCallback(() => {
+    dispatch(F.changeEmail(D.randomEmail()))
+  }, [dispatch])
+  const changePicture = useCallback(
+    () => dispatch(F.changePicture({large: D.randomAvatar()})),
+    [dispatch]
+  )
+
+  useEffect(getRemoteUser, [getRemoteUser])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>RemoteUserTest</Title>
+      <div className="flex justify-center mt-4">
+        <Button className="btn-sm btn-primary" onClick={getRemoteUser}>
+          GET REMOTE USER
+        </Button>
+        <Button className="ml-4 btn-sm btn-accent" onClick={changeName}>
+          CHANGE NAME
+        </Button>
+        <Button className="ml-4 btn-sm btn-success" onClick={changeEmail}>
+          CHANGE EMAIL
+        </Button>
+        <Button className="ml-4 btn-sm btn-secondary" onClick={changePicture}>
+          CHANGE PICTURE
+        </Button>
+      </div>
+      {loading && (
+        <div className="flex items-center justify-center">
+          <Button className="btn-circle loading"></Button>
+        </div>
+      )}
+      {errorMessage.length && (
+        <div className="p-4 mt-4 bg-red-200">
+          <p className="text-3xl text-red-500 text-bold">{errorMessage}</p>
+        </div>
+      )}
+      <div className="flex justify-center p-4 mt-4">
+        <Avatar src={user.picture.large} />
+        <div className="ml-4">
+          <p className="text-xl text-bold">
+            {user.name.title}. {user.name.first} {user.name.last}
+          </p>
+          <p className="italic text-gray-600">{user.email}</p>
+        </div>
+      </div>
     </section>
   )
 }
